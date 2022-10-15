73caf45df0d1796b2b3134b65cf4fbae235e7e1b
123 <makaryan.vika98@mail.ru>
2022-10-15 23:05:27 +0300

    Lesson2

---
+++ HomeWorkApp/src/main/java/main/HomeWorkApp.java
@@ -0,0 +1,18 @@
+package main;
+
+
+                public class HomeWorkApp {
+                public static void main(String[] args) {
+                    printThreeWords();
+
+                }
+                    public static void printThreeWords(){
+                    printThreeWords ("Orange");
+                    printThreeWords ("Banana");
+                    printThreeWords ("Aplle");
+                    }
+
+
+                }
+
+
